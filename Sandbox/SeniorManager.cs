14e7a0c25633a061f31d278d0666ea28367803c6
Dshy <KortSkg@KORTSKG-PC>
2016-11-14 13:40:38 +0100

    Tilføet senior manager

---
+++ Sandbox/SeniorManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sandbox
+{
+    class SeniorManager : Manager
+    {
+        private int performancelevel;
+
+        public SeniorManager(string name, int performancelevel) : base(name)
+        {
+            this.performancelevel = performancelevel;
+        }
+        public override bool IsBonusPaidOut()
+        {
+            if (performancelevel >= 6)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
