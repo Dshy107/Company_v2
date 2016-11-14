10fa9f33f66f1dbac1ae025cfb2180c1bff16b9d
Dshy <KortSkg@KORTSKG-PC>
2016-11-14 13:51:25 +0100

    I gang med insertcodehere

--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -10,6 +10,11 @@ namespace Sandbox
             // The FIRST line of code should be BELOW this line
 
             // Insert your tests here...
+            var allEmployees = new List<Employee>();
+            Worker worker1 = new Worker("Bill");
+            allEmployees.Add(worker1);
+            JuniorManager JM = new JuniorManager("Rodney", 185);
+           
 
 
             // The LAST line of code should be ABOVE this line
