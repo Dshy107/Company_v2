c572af2a094bcd63da4294a66b7ed871c9e1a553
Dshy <KortSkg@KORTSKG-PC>
2016-11-14 12:49:21 +0100

    Tilføjet worker class og arvet fra employee

---
+++ Sandbox/Worker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sandbox
+{
+    class Worker : Employee
+    {
+        private int workHoures;
+        private int payPrHour;
+        public Worker(string name) : base(name)
+        {
+        }
+
+        public override int GetBonusPerMonth()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int GetSalaryPerMonth()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsBonusPaidOut()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
