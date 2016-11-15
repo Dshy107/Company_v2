0f56cb0f68ea38cffa632385cf294a31ca335fda
Dshy <KortSkg@KORTSKG-PC>
2016-11-15 09:21:53 +0100

    Tilføjet worker, senior og junior manager til en liste

--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -14,9 +14,13 @@ namespace Sandbox
             Worker worker1 = new Worker("Bill");
             allEmployees.Add(worker1);
             JuniorManager JM = new JuniorManager("Rodney", 185);
-           
-
-
+            JuniorManager JM2 = new JuniorManager("Troy", 175);
+            allEmployees.Add(JM);
+            allEmployees.Add(JM2);
+            SeniorManager SM = new SeniorManager("James", 7);
+            SeniorManager SM2 = new SeniorManager("Carlos", 5);
+            allEmployees.Add(SM);
+            allEmployees.Add(SM2);
             // The LAST line of code should be ABOVE this line
         }
     }
